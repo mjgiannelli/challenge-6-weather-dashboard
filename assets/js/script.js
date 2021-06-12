840797bf06968dde5a80fba50da8a0f661c39ca3
Mjgiannelli <51857101+mjgiannelli@users.noreply.github.com>
2021-06-12 10:55:11 -0500

    add pseudo code to js file

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+// ** START PSEUDO CODE (subject to change) ** //
+
+// when user searches for a city (clicks search button):
+//  - store the user input in a variable
+//  - use a fetch api to get the current & future conditions for that city
+//  - store that city into local storage
+// use the data from fetch to populate in the current-weather container:
+//  - name and today's date as M/DD/YYY
+//  - temp
+//  - wind
+//  - humidity
+//  - UV index (color coded for favorable(green), moderate(yellow), or severe(red))
+// use the data from fetch to populate in the five-day container:
+//  - date
+//  - an icon reprsentation of weather conditions
+//  - the temp
+//  - wind speed
+//  - humidity
+// use data in local.storage to create a button under the <hr> in search area for city history
+//  - when you click the button it displays the current and future conditions for that city
+
+// ** END PSEUDO CODE (subject to change) ** //
